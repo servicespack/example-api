f6b728886b8f304e6866a666ccad62369786471a
Gabriel Rufino <contato@gabrielrufino.com>
2023-10-02 18:43:57 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import http from 'node:http';
+import { createServer } from 'node:http';
 
-const server = http.createServer((request, response) => {
+const server = createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
   response.write('Hello World!');
   response.end();
